57e7beb00ac0beecc75b042817011db4b53068bd
Kevnlan <kevnlan11@gmail.com>
2026-06-20 17:06:10 +0300

    feat: phase one done

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,11 @@ import { AuthModule } from './modules/auth/auth.module';
 import { CreatorModule } from './modules/creator/creator.module';
 import { BrandModule } from './modules/brand/brand.module';
 import { CampaignModule } from './modules/campaign/campaign.module';
+import { CountryModule } from './modules/country/country.module';
+import { KycModule } from './modules/kyc/kyc.module';
+import { WalletModule } from './modules/wallet/wallet.module';
+import { WithdrawalModule } from './modules/withdrawal/withdrawal.module';
+import { TaxModule } from './modules/tax/tax.module';
 import configuration from './config/configuration';
 
 @Module({
@@ -17,6 +22,11 @@ import configuration from './config/configuration';
     CreatorModule,
     BrandModule,
     CampaignModule,
+    CountryModule,
+    KycModule,
+    WalletModule,
+    WithdrawalModule,
+    TaxModule,
   ],
 })
 export class AppModule {}
